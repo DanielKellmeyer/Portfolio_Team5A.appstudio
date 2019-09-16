968de5af9948344743f394e588061087a1e92a45
Daniel Kellmeyer <dsk96139@creighton.edu>
2019-09-15 22:48:44 -0500

    Slightly Debugged
    Popups are still showing where they shouldn't

--- forms/TheBigAdventure/TheBigAdventure.js
+++ forms/TheBigAdventure/TheBigAdventure.js
@@ -16,29 +16,37 @@ movement['Left'] = 2
 movement['Right'] = 3
 movement['Backward'] = 4
 
+//Creating blank array to hold placement and to hold the length of my array
+let place = ['Start']
+let plength = place.length 
+
 livingRoom = prompt('You have started at the entrance. Type 1,2,3, or 4 to enter your first move')
-if (livingRoom == movement['Left'] || livingRoom == movement['Right'] || livingRoom == movement['Backward']) {
+if (livingRoom == movement['Left'] || livingRoom == movement['Right'] || livingRoom == movement['Backward'] ) {
   alert('You have ran into a wall.')
-} else if(livingRoom == movement['Forward']){
+} else {
   alert('You have found the Living Room!')
+  place.push('Living Room')
   }
-closet = prompt('Congratulations! Type 1,2,3, or 4 to enter your next move.')
-if (closet == movement['Left'] || closet == movement['Forward'] || closet == movement['Backward']) {
+closet = prompt('Congratulations, you found the Living Room! Type 1,2,3, or 4 to enter your next move.')
+if (plength = 1 && closet == movement['Left'] || closet == movement['Forward'] || closet == movement['Backward']) {
   alert('You have ran into a wall.')
-} else if(closet == movement['Right']){
+} else {
   alert('You have found the Closet!')
+   place.push('Closet')
   }
-bathroom = prompt('Congratulations! Type 1,2,3, or 4 to enter your next move.')
-if (bathroom == movement['Right'] || bathroom == movement['Forward'] || bathroom == movement['Backward']) {
+bathroom = prompt('Congratulations, you found the Closet! Type 1,2,3, or 4 to enter your next move.')
+if (plength = 2 && bathroom == movement['Right'] || bathroom == movement['Forward'] || bathroom == movement['Backward']) {
   alert('You have ran into a wall.')
-} else if(bathroom == movement['Left']){
+} else {
   alert('You have found the Bathroom!')
+   place.push('Bathroom')
   }
-kitchen = prompt('Congratulations! Type 1,2,3, or 4 to enter your next move.')
-if (kitchen == movement['Right'] || kitchen == movement['Forward'] || kitchen == movement['Left']) {
+kitchen = prompt('Congratulations, you found the Bathroom! Type 1,2,3, or 4 to enter your next move.')
+if (plength = 3 && kitchen == movement['Right'] || kitchen == movement['Forward'] || kitchen == movement['Left']) {
   alert('You have ran into a wall.')
-} else if(kitchen == movement['Backward']){
+} else {
   alert('You have found the Kitchen!')
+   place.push('Kitchen')
   }
   //This is the last room so I used a switch in this case
 var lastRoom = prompt("This is your final move to win the game! Choose carefully :)!")
